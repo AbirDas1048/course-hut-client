1120850e54b686789666907be66ff7b883b0ba2b
Abir Das <abirdas422@gmail.com>
2022-10-27 21:47:51 +0600

    Main page update

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,12 +1,10 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Outlet } from 'react-router-dom';
-import { AuthContext } from '../context/AuthProvider';
 import Footer from '../pages/Shared/Footer';
 import Header from '../pages/Shared/Header';
 import './Main.css';
 
 const Main = () => {
-    const { mood } = useContext(AuthContext);
     return (
         <div>
             <Header></Header>
