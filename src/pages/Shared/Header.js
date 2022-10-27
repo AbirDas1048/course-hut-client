01eed1848588e084f97ba0d03eabcab8c0ae8e34
Abir Das <abirdas422@gmail.com>
2022-10-27 21:39:30 +0600

    Navbar update

--- src/pages/Shared/Header.js
+++ src/pages/Shared/Header.js
@@ -3,18 +3,19 @@ import { Container, Form, Nav, Navbar } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthProvider';
 import './Header.css';
+import logo from '../../images/logo.png'
 
 const Header = () => {
 
     const { mood, setMood } = useContext(AuthContext);
 
     return (
-        <Navbar collapseOnSelect sticky='top' expand="lg" bg="light" variant='light'>
+        <Navbar collapseOnSelect sticky='top' expand="lg" bg={mood ? "light" : "dark"} variant={mood ? "light" : "dark"}>
             <Container>
                 <Navbar.Brand href="/">
                     <img
                         alt=""
-                        src=""
+                        src={logo}
                         width="30"
                         height="30"
                         className="d-inline-block align-top"
@@ -25,16 +26,17 @@ const Header = () => {
                 <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
 
                     <Nav >
-                        <NavLink className="nav-link {({ isActive }) => isActive ? 'active' : undefined}" to="/home">Home</NavLink>
-                        <NavLink className="nav-link {({ isActive }) => isActive ? 'active' : undefined}" to='/courses'>Courses</NavLink>
-                        <NavLink className="nav-link {({ isActive }) => isActive ? 'active' : undefined}" to="/faq">FAQ</NavLink>
-                        <NavLink className="nav-link {({ isActive }) => isActive ? 'active' : undefined}" to="/blog">Blog</NavLink>
+                        <NavLink className="nav-link {({ isActive }) => isActive ? 'navActive' : undefined}" to="/home">Home</NavLink>
+                        <NavLink className="nav-link {({ isActive }) => isActive ? 'navActive' : undefined}" to='/courses'>Courses</NavLink>
+                        <NavLink className="nav-link {({ isActive }) => isActive ? 'navActive' : undefined}" to="/faq">FAQ</NavLink>
+                        <NavLink className="nav-link {({ isActive }) => isActive ? 'navActive' : undefined}" to="/blog">Blog</NavLink>
                     </Nav>
                     <Form className='mx-2'>
                         <Form.Check onChange={() => setMood(!mood)}
                             type="switch"
                             id="custom-switch"
                             label={mood ? "Light" : "Dark"}
+                            className={mood ? "text-dark" : "text-light"}
                         />
                     </Form>
                 </Navbar.Collapse>
